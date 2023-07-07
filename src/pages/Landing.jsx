174861216b32c1ea7d457a2640dc34531e49f332
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-07 16:32:42 -0400

    add CocktailList and CocktailCard

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
+import CocktailList from "../components/CocktailList";
 
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
@@ -11,9 +12,12 @@ export const loader = async () => {
 };
 
 const Landing = () => {
-  const {drinks, searchTerm} = useLoaderData();
-  console.log(drinks);
+  const { drinks, searchTerm } = useLoaderData();
 
-  return <h3>Landing</h3>;
+  return (
+    <>
+      <CocktailList drinks={drinks}/>
+    </>
+  );
 };
 export default Landing;
